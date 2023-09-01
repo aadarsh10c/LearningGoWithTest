7ea2c951f5fcc5cabbe7f076280ec5c44906c2f8
Adarsh Kumar <adarsh010c@gmail.com>
2023-09-01 23:02:53 +0530

    prettier configured

--- struct/perimeter_test.go
+++ struct/perimeter_test.go
@@ -2,40 +2,37 @@ package structs
 
 import "testing"
 
-
-
 func TestPremieter(t *testing.T) {
-	rectangle:=Rectangle{10.0, 10.0}
-	got:= Perimeter(rectangle)
-	want:=40.0
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
 
-	if got != want{
+	if got != want {
 		t.Errorf("got %g, want %g", got, want)
 	}
 
-	
 }
 
-func TestArea(t *testing.T){
-	
+func TestArea(t *testing.T) {
+
 	//check area
-	checkArea:=func (t *testing.T, shape Shape, want float64){
+	checkArea := func(t *testing.T, shape Shape, want float64) {
 		t.Helper()
-		got:= shape.Area()
-		if got != want{
+		got := shape.Area()
+		if got != want {
 			t.Errorf("got %g, want %g", got, want)
 		}
 	}
-	t.Run("rectangles",func(t *testing.T) {
-		rectangle:=Rectangle{12.0,6.0}
-		want:= 72.0
-		checkArea(t,rectangle,want)
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		want := 72.0
+		checkArea(t, rectangle, want)
 	})
 
-	t.Run("circles",func(t *testing.T) {
-		circle:=Circle{10.0}
-		want:=314.1592653589793
-		checkArea(t,circle,want)
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		want := 314.1592653589793
+		checkArea(t, circle, want)
 	})
-	
+
 }
