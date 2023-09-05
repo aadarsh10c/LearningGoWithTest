d4ef7af9be87d4a85323939a330e41f918e19c72
Adarsh Kumar <adarsh010c@gmail.com>
2023-09-06 02:24:37 +0530

    error assertion test complete

--- pointers_errors/wallet.go
+++ pointers_errors/wallet.go
@@ -1,15 +1,41 @@
 package wallet
 
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
 // method to deposit money in wallet
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance = w.balance + amount
 }
 
 // method to check balance
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+var errInsuffucientfunds = errors.New("cannot withdraw, insufficient funds")
+
+// method to withdraw
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errInsuffucientfunds
+	}
+	w.balance = w.balance - amount
+	return nil
 }
--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -5,11 +5,51 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
-	got := wallet.Balance()
-	want := 10
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(10)
+		assertEqual(got, want, t)
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{balance: startingBalance}
+		wallet.Withdraw(Bitcoin(120))
+		got := wallet.Balance()
+		want := Bitcoin(10)
+		assertEqual(got, want, t)
+
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startBalance := Bitcoin(20)
+		wallet := Wallet{balance: startBalance}
+		err := wallet.Withdraw(Bitcoin(120))
+		got := wallet.Balance()
+		want := Bitcoin(20)
+
+		assertError(err, errInsuffucientfunds, t)
+		assertEqual(got, want, t)
+
+	})
+
+}
+
+func assertError(got error, want error, t *testing.T) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("Wanted an error but did n't get one")
+	}
 	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertEqual(got Bitcoin, want Bitcoin, t *testing.T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %s, wanted %s", got, want)
 	}
 }
