0a0a01922df352c10c5d8354f9d95fe75d563f3d
Adarsh Kumar <adarsh010c@gmail.com>
2023-09-06 16:39:27 +0530

    Search function complete, add dunction test added

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,19 @@
 package maps
 
-func Search(dictionary map[string]string, key string) string {
-	return dictionary[key]
+import (
+	"errors"
+	"fmt"
+)
+
+// create a dictionary type
+type Dictionary map[string]string
+var ErrNotFound = errors.New("could not find the word you were looking for")
+// create a method for our dictionary
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	fmt.Printf("value: %q of %T \n", definition, definition)
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,14 +3,52 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
-	got := Search(dictionary, "test")
+	//create adn instance of dictinary, woth values
+	dictionary := Dictionary{"test": "this is just a test"}
+	t.Run("known word", func(t *testing.T) {
+		//call the method on dictionary instance and record value
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		//test the response
+		assertStrings(got, want, t)
+	})
+
+	t.Run("unkown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := ErrNotFound
+		//i error is not given
+		if err == nil {
+			t.Fatal("Word is present in the dictionary")
+		}
+		assertError(err, want, t)
+	})
+
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
 	want := "this is just a test"
+	got, err := dictionary.Search("test")
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
 
 	assertStrings(got, want, t)
 }
 
-func assertStrings(got string, want string, t *testing.T) {
+func assertError(got, want error, t *testing.T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, wanted %q, given %q", got, want, "test")
+
+	}
+}
+
+func assertStrings(got, want string, t *testing.T) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q, wanted %q, given %q", got, want, "test")
