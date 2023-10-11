eb73aa0c06ab0fb7b0ebb73b4a408f90c5f66cdb
Adarsh Kumar <adarsh010c@gmail.com>
2023-10-11 15:27:50 +0530

    added the stringer interface

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -3,14 +3,14 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{20}
+	wallet := Wallet{Bitcoin(20)}
 
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	got := wallet.Balance()
-	want := 30
+	want := Bitcoin(30)
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
