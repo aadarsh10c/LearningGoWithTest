9d770105d101a7644b9685975e402901dee2498e
aadarsh010c <adarsh010c@gmail.com>
2024-01-09 16:38:24 +0530

    added

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	d "github.com/aadarsh10c/LearningGoWithTest/dinjection"
+
+	"net/http"
 )
 
 func main() {
-	d.Greet(os.Stdout, "Elodie")
+	d.Greet(os.Stdout, "Test")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(d.MyGreeterHandler)))
 }
