30ad8d73b6d8e4cd30dcc9caafdfa578db176acd
aadarsh010c <adarsh010c@gmail.com>
2024-01-10 21:45:52 +0530

    added becnhmark

--- concurrency/webSiteChecker.go
+++ concurrency/webSiteChecker.go
@@ -1,13 +1,11 @@
 package concurrency
 
-type WebsiteChecker interface {
-	CallBack(url string) bool
-}
+type WebsiteChecker func(string) bool
 
-func CheckWebsites(c WebsiteChecker, urls []string) map[string]bool {
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	result := make(map[string]bool)
 	for _, url := range urls {
-		result[url] = c.CallBack(url)
+		result[url] = wc(url)
 	}
 	return result
 }
--- concurrency/websiteChecker_test.go
+++ concurrency/websiteChecker_test.go
@@ -3,30 +3,46 @@ package concurrency
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
-type testWebsiteCallback struct{}
-
-func (t *testWebsiteCallback) CallBack(url string) bool {
+func mockWebsiteCheck(url string) bool {
 	return url != "waat://furhurterwe.geds"
 }
 
 func TestCheckWebsites(t *testing.T) {
-	websites := []string{
-		"http://google.com",
-		"http://blog.gypsydave5.com",
-		"waat://furhurterwe.geds",
-	}
-	t.Run("Websites map", func(t *testing.T) {
-		test := &testWebsiteCallback{}
-		got := CheckWebsites(test, websites)
+	t.Run("Check websites", func(t *testing.T) {
+		websites := []string{
+			"http://google.com",
+			"http://blog.gypsydave5.com",
+			"waat://furhurterwe.geds",
+		}
+		got := CheckWebsites(mockWebsiteCheck, websites)
 		want := map[string]bool{
 			"http://google.com":          true,
 			"http://blog.gypsydave5.com": true,
 			"waat://furhurterwe.geds":    false,
 		}
-		if !reflect.DeepEqual(want, got) {
+
+		if !reflect.DeepEqual(got, want) {
 			t.Fatalf("wanted %v, got %v", want, got)
 		}
+
 	})
+}
+
+func slowStubWebsiteChecket(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < 100; i++ {
+		urls[i] = "a true"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecket, urls)
+	}
 }
